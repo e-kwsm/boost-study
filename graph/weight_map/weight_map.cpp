8c413af3332e300f7aeec320327e0258604fc63a
E Kawashima <e-kwsm@users.noreply.github.com>
2017-06-16 19:48:03 +0900

    add custom dijkstra visitor

--- graph/weight_map/weight_map.cpp
+++ graph/weight_map/weight_map.cpp
@@ -7,6 +7,7 @@
 #include <boost/graph/graph_utility.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/graphviz.hpp>
+#include <boost/optional/optional.hpp>
 #include <boost/property_map/function_property_map.hpp>
 #include <boost/range/iterator_range_core.hpp>
 
@@ -41,6 +42,28 @@ using Graph = boost::adjacency_list<
     boost::no_property, // GraphProperties
     boost::listS        // EdgeList
     >;
+
+struct reach_threshold {};
+
+struct threshold_visitor : boost::default_dijkstra_visitor
+{
+   threshold_visitor(decltype(Edge::weight) threshold): threshold{threshold}, total_weight{0} {}
+
+   void examine_edge(Graph::edge_descriptor e, const Graph& g)
+   {
+       min_weight = min_weight ? std::min(*min_weight, g[e].weight) : g[e].weight;
+   }
+   void finish_vertex(Graph::vertex_descriptor v, const Graph& g)
+   {
+       if (min_weight) total_weight += *min_weight;
+       if (total_weight >= threshold) throw reach_threshold{};
+       min_weight = boost::none;
+   }
+
+   const decltype(Edge::weight) threshold;
+   decltype(Edge::weight) total_weight;
+   boost::optional<decltype(Edge::weight)> min_weight;
+};
 
 int main()
 {
@@ -101,7 +124,35 @@ int main()
         }
     };
 
+    auto output_distances_threshold = [&](Graph::vertex_descriptor v, unsigned threshold)
+    {
+        std::cout << "# boost::dijkstra_shortest_paths w/ custom visitor\n";
+        std::vector<decltype(Edge::weight)> distances(boost::num_vertices(g));
+        try
+        {
+            boost::dijkstra_shortest_paths(g, v,
+                    boost::weight_map(
+                        boost::make_function_property_map<Graph::edge_descriptor>(
+                            [&](const Graph::edge_descriptor& e)->decltype(Edge::weight)
+                            { return g[e].weight; }
+                            )
+                        ).distance_map(&distances[0])
+                    .visitor(threshold_visitor{threshold}));
+        }
+        catch (reach_threshold)
+        {}
+
+        for (auto i : boost::make_iterator_range(vertices(g)))
+        {
+            auto d = distances[i];
+            if (d == std::numeric_limits<decltype(Edge::weight)>::max())
+                std::cout << '#';
+            std::cout << g[v].name << " - " << g[i].name << "\t" << d << '\n';
+        }
+    };
+
     output_distances(a);
+    output_distances_threshold(a, 2);
 
 #ifdef GRAPHVIZ
     std::string fname = "graph.dot";
